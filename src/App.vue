10ae6a62ac8bb4aedd5b8655cd1f06dde4f7f972
Claudio <claudiojlf@gmail.com>
2022-01-03 17:56:00 -0300

    avigation bar structure built

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<NavigationBar />
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+		NavigationBar
 	}
 }
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="navigation-bar">
+		<ul>
+			<li v-for="nav in navigationList" :key="nav.id">
+				<component :is="nav.id" />
+				<p>{{ nav.label }}</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import burguerIcon from '../assets/icons/food.svg'
+import comboIcon from '../assets/icons/french-fries.svg'
+import desertIcon from '../assets/icons/ice-cream.svg'
+import pizzaIcon from '../assets/icons/pizza.svg'
+import drinksIcon from '../assets/icons/smothie.svg'
+
+export default {
+	name: 'NavigationBar',
+	components: {
+		burguerIcon,
+		comboIcon,
+		desertIcon,
+		pizzaIcon,
+		drinksIcon
+	},
+	data() {
+		return {
+			navigationList: [
+				{ label: 'Pizza', id: 'pizzaIcon' },
+				{ label: 'Bebidas', id: 'drinksIcon' },
+				{ label: 'Sobremesas', id: 'desertIcon' },
+				{ label: 'Combos', id: 'comboIcon' },
+				{ label: 'Burguer', id: 'burguerIcon' }
+			]
+		}
+	}
+}
+</script>
+
+<style></style>
